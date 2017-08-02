f0b8467bda1821e3a924ed00268cdc17e8809d13
BaconBravo <fizaguirre91@gmail.com>
2017-08-01 23:13:34 -0700

    Initial Commit

---
+++ cquest_mvp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cquest_mvp
+//
+//  Created by Francisco Izaguirre on 8/1/17.
+//  Copyright © 2017 BaconBravo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
